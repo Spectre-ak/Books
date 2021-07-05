c375c2ef66f82d4fd33057b3b8e888b04dc717d7
Spectre-ak <62694340+Spectre-ak@users.noreply.github.com>
2021-07-05 16:56:43 +0530

    base app

---
+++ src/main/java/com/books/fetch/FetchApplication.java
@@ -0,0 +1,13 @@
+package com.books.fetch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FetchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FetchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/books/fetch/FetchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.books.fetch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FetchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
