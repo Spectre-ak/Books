59d7cbcd7b160aa0b2f9740f388c3ab6d941125c
Spectre-ak <62694340+Spectre-ak@users.noreply.github.com>
2021-07-06 17:14:50 +0530

    Update FetchApplication.java

--- src/main/java/com/books/fetch/FetchApplication.java
+++ src/main/java/com/books/fetch/FetchApplication.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.sun.beans.util.Cache;
 
 @SpringBootApplication
 public class FetchApplication {
